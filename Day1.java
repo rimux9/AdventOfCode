0991e9471ca028cbe54361c1b21bf36729a9ace3
rimux9 <rmatuliokas@gmail.com>
2020-11-07 19:14:36 +0200

    day1

---
+++ Day1.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+
+        List<String> input;
+        input = FileUtil.loadFile("src/day1");
+        int sum = 0;
+        for (String s : input) {
+
+
+            int mass = Integer.parseInt(s);
+            //part2
+            while (mass > 0) {
+                int result = (mass / 3) - 2;
+                if (result > 0)
+            sum += result;
+                mass = result;
+
+        }
+        }
+            System.out.println(sum);
+        }
+    }
+
